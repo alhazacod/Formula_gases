b41e3e527d045d7be881d88f4bf6023bee9fb4d3
jenkko <angel_01@hotmail.es>
2017-04-18 23:06:23 -0500

    1.0
    solamente se puede resolver problemas con volumen y presion.
    Problemas a arreglar:
    1-agregar el poder visualizar la formula.
    2-mejorar el estilo visual.

---
+++ Formula_gases/Formula_gases/Ley_boyle.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formula_gases
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ley_boyle : ContentPage
+    {
+        public Ley_boyle()
+        {
+
+            InitializeComponent();
+
+            buttonResolver.Clicked += ButtonResolver_Clicked;
+        }
+
+        private void ButtonResolver_Clicked(object sender, EventArgs e)
+        {
+            double v1;
+            double v2;
+            double p1;
+            double p2;
+            try
+            {
+                if (string.IsNullOrEmpty(entryV1.Text))//deseamos obtener el valor de v1
+                {
+
+                    if (!string.IsNullOrEmpty(entryV2.Text) &&
+                       !string.IsNullOrEmpty(entryP1.Text) &&
+                       !string.IsNullOrEmpty(entryP2.Text))
+                    {
+                        try
+                        {
+                            v2 = Convert.ToDouble(entryV2.Text);
+                            p1 = Convert.ToDouble(entryP1.Text);
+                            p2 = Convert.ToDouble(entryP2.Text);
+                            v1 = (v2 * p2) / p1;
+                            entryV1.Text = v1.ToString();
+                        }
+                        catch
+                        {
+                            DisplayAlert("Ha ocurrido un error", "Por favor verifica que todos los campos esten escritos correctamente", "Aceptar");
+                        }
+                    }
+                    else
+                    {
+                        DisplayAlert("Ha ocurrido un error", "Solo puedes dejar un espacio en blanco", "Aceptar");
+                    }
+                }//deseamos obtener el valor de v1
+                else if (string.IsNullOrEmpty(entryV2.Text))//deseamos obtener el valor de v2
+                {
+                    v1 = Convert.ToDouble(entryV1.Text);
+                    p1 = Convert.ToDouble(entryP1.Text);
+                    p2 = Convert.ToDouble(entryP2.Text);
+                    if (!string.IsNullOrEmpty(entryV1.Text) &&
+                       !string.IsNullOrEmpty(entryP1.Text) &&
+                       !string.IsNullOrEmpty(entryP2.Text))
+                    {
+                        try
+                        {
+                            v2 = (v1 * p1) / p2;
+                            entryV2.Text = v2.ToString();
+                        }
+                        catch
+                        {
+                            DisplayAlert("Ha ocurrido un error", "Por favor verifica que todos los campos esten escritos correctamente", "Aceptar");
+                        }
+                    }
+                    else
+                    {
+                        DisplayAlert("Ha ocurrido un error", "Solo puedes dejar un espacio en blanco", "Aceptar");
+                    }
+                }//deseamos obtener el valor de v2
+                else if (string.IsNullOrEmpty(entryP1.Text))//deseamos obtener el valor de p1
+                {
+                    v1 = Convert.ToDouble(entryV1.Text);
+                    v2 = Convert.ToDouble(entryV2.Text);
+                    p2 = Convert.ToDouble(entryP2.Text);
+                    if (!string.IsNullOrEmpty(entryV1.Text) &&
+                       !string.IsNullOrEmpty(entryV2.Text) &&
+                       !string.IsNullOrEmpty(entryP2.Text))
+                    {
+                        try
+                        {
+                            p1 = (v2 * p2) / v1;
+                            entryP1.Text = p1.ToString();
+                        }
+                        catch
+                        {
+                            DisplayAlert("Ha ocurrido un error", "Por favor verifica que todos los campos esten escritos correctamente :V", "Aceptar");
+                        }
+                    }
+                    else
+                    {
+                        DisplayAlert("Ha ocurrido un error", "Solo puedes dejar un espacio en blanco", "Aceptar");
+                    }
+                }//deseamos obtener el valor de p1
+                else if (string.IsNullOrEmpty(entryP2.Text))//deseamos obtener el valor de p2
+                {
+                    v1 = Convert.ToDouble(entryV1.Text);
+                    v2 = Convert.ToDouble(entryV2.Text);
+                    p1 = Convert.ToDouble(entryP1.Text);
+                    if (!string.IsNullOrEmpty(entryV1.Text) &&
+                       !string.IsNullOrEmpty(entryV2.Text) &&
+                       !string.IsNullOrEmpty(entryP1.Text))
+                    {
+                        try
+                        {
+                            p2 = (v1 * p1) / v2;
+                            entryP2.Text = p2.ToString();
+                        }
+                        catch
+                        {
+                            DisplayAlert("Ha ocurrido un error", "Por favor verifica que todos los campos esten escritos correctamente", "Aceptar");
+                        }
+                    }
+                    else
+                    {
+                        DisplayAlert("Ha ocurrido un error", "Solo puedes dejar un espacio en blanco", "Aceptar");
+                    }
+                }//deseamos obtener el valor de p2
+                else
+                {
+                    DisplayAlert("Ha ocurrido un error", "Por favor verifica que todos los campos esten escritos correctamente", "Aceptar");
+                }
+            }
+            catch
+            {
+                DisplayAlert("Error","Por favor revisa que los valores sean solo numeros","Ok");
+            }
+        }
+    }
+}
---
+++ Formula_gases/Formula_gases/burgerMenu.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formula_gases
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class burgerMenu : MasterDetailPage
+    {
+        public burgerMenu()
+        {
+            InitializeComponent();
+
+            Init();
+
+            listMenu.ItemSelected += ListMenu_ItemSelected;
+        }
+
+        
+
+        private void Init()
+        {
+            List<Menu> menu = new List<Menu>
+            {
+                new Menu{page=new MainPage(), menuTitle="Inicio", menuDetail="Regresar a pagina principal"},
+                new Menu{page=new Ley_boyle(), menuTitle="Ley de Boyle", menuDetail="V1*P1=V2*P2"}
+            };
+
+            listMenu.ItemsSource = menu;
+
+            listMenu.Margin = new Thickness(0, 24, 0, 0);
+
+            Detail = new NavigationPage(new MainPage());
+        }
+        private void ListMenu_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var menu = e.SelectedItem as Menu;
+            var pagina = menu.page;
+            IsPresented = false;
+            Detail = new NavigationPage(menu.page);
+        }
+        internal class Menu
+        {
+            public string menuTitle
+            {
+                get;
+                set;
+            }
+            public string menuDetail
+            {
+                get;
+                set;
+            }
+            public Page page
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
+
